6cb617d641b62d1145f1f42cd50a5b2cec1a2132
Greiner <soren@greiner.dk>
2018-02-05 22:27:10 +0100

    Added car control example

---
+++ ev3dev-c-master/eg/car/car.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "brick.h"
+
+
+int g_bAlive = true; ///< Program is alive
+int g_buttons = IR_REMOTE__NONE_;
+
+#define MOTOR_LEFT     OUTC
+#define MOTOR_RIGHT    OUTB
+#define MOTOR_BOTH     ( MOTOR_LEFT | MOTOR_RIGHT )
+
+#define IR_CHANNEL     0 
+
+int max_speed;         /* Motor maximal speed (will be detected) */ 
+POOL_T ir;             /* IR sensor port (will be detected) */
+
+float speed = 0.0f;
+float angle = 0.0f;
+
+
+int init( void )
+{
+	if ( tacho_is_plugged( MOTOR_BOTH, TACHO_TYPE__NONE_ )) {  /* any type of motor */
+		max_speed = tacho_get_max_speed( MOTOR_LEFT, 0 );
+		tacho_reset( MOTOR_BOTH );
+	} else {
+		printf( "Please, plug LEFT motor in C port,\n"
+		"RIGHT motor in B port and try again.\n"
+		);
+		/* Inoperative without motors */
+		return ( 0 );
+	}
+
+	ir = sensor_search( LEGO_EV3_IR );
+	if ( ir ) {
+		ir_set_mode_ir_remote( ir );
+
+		printf(	"IR remote control:\n"
+		"RED UP   & BLUE UP   - forward\n"
+		"RED DOWN & BLUE DOWN - backward\n"
+		"RED UP   | BLUE DOWN - left\n"
+		"RED DOWN | BLUE UP   - right\n"
+		);
+	} else {
+		printf( "IR sensor is NOT found.\n"
+		"Please, use the EV3 brick buttons.\n"
+		);
+	}
+	printf( "Press BACK on the EV3 brick for EXIT...\n" );
+
+	return ( 1 );
+}
+
+
+void UpdateBrick()
+{
+	uint8_t keys;
+	uint8_t pressed = EV3_KEY__NONE_;
+
+	keys = brick_keys();
+
+	switch(keys)
+	{
+		case EV3_KEY_BACK:
+			g_bAlive = false;
+		break;
+
+		default:
+		break;
+	}
+}
+
+
+void UpdateIr()
+{
+	if ( ir == SOCKET__NONE_ )
+	{
+		return;
+	}
+
+	bool bSpeedChanged = false;
+	int buttons;
+	buttons = sensor_get_value( IR_CHANNEL, ir, IR_REMOTE__NONE_ );
+
+	if(buttons != g_buttons)
+	{
+		g_buttons = buttons;
+
+		switch( buttons )
+		{
+			case RED_UP_BLUE_UP:
+				break;
+	
+			case RED_DOWN_BLUE_DOWN:
+				break;
+	
+			case RED_UP:
+				if(speed < 100.0)
+				{
+					speed += 5.0f;
+				}
+				break;
+	
+			case RED_UP_BLUE_DOWN:
+				break;
+	
+			case BLUE_DOWN:
+				break;
+	
+			case BLUE_UP:
+				break;
+	
+			case RED_DOWN_BLUE_UP:
+				break;
+	
+			case RED_DOWN:
+				if(speed > -100.0)
+				{
+					speed -= 5.0f;
+				}
+				break;
+	
+			case IR_REMOTE__NONE_:
+			case RED_UP_RED_DOWN:
+			case BLUE_UP_BLUE_DOWN:
+			case BEACON_MODE_ON:
+				break; 
+		}
+
+	}
+
+	if(bSpeedChanged)
+	{
+		printf("speed %f\n", speed);
+
+		int motorSetpoint = (int)(max_speed * speed / 100); 
+		tacho_set_speed_sp( MOTOR_BOTH, motorSetpoint );
+		tacho_run_forever( MOTOR_BOTH ); 		
+	}
+}
+
+
+void UpdateDrive()
+{
+}
+
+
+
+int main( void )
+{
+	printf( "Waiting the EV3 brick online...\n" );
+	if ( !brick_init()) return ( 1 );
+
+	printf( "*** ( EV3 ) Hello! ***\n" );
+	alive = init();
+
+	while ( g_bAlive ) 
+	{
+		UpdateBrick();
+		UpdateIr();
+		UpdateDrive();
+		sleep_ms(10);
+	}
+	brick_uninit();
+
+	printf( "*** ( EV3 ) Bye! ***\n" );
+	return ( 0 );
+}
